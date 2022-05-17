fa5db08d317570269cc51690ac07e266884f7cc1
GGN_2015 <630230141@qq.com>
2022-05-17 17:26:36 +0800

    GPR, JMP

--- mips.v
+++ mips.v
@@ -22,10 +22,52 @@
  */
 
 
+ //! -------------------- 待解决的问题 -------------------- !//
+ //? 指令发射的同时，如果所需要使用的数据正在上 CDB，可能会错过读取机会
+ //? 取指令条件：当前没有未发射的指令，当前 JMP 模块不工作，当前 CDB 上的源设备不是 JMP
+
+
+// -------------------- 已经解决的问题 -------------------- //
+//
+
 `timescale 1ns/10ps
 
 
+// -------------------- 组合逻辑：CDB 获取模块 -------------------- //
+module FetchCDB (
+    // 以下端口来自 CDB 的输出
+    input             cdb_buzy,
+    input      [2 :0] cdb_device,
+    input      [31:0] cdb_value,
+
+    // 以下端口为即将被传输的数据，可能就绪，可能未就绪
+    input      [35:0] buffer_value,
+
+    // 以下端口为经过读取 CDB 而分析得到的数据
+    output reg [35:0] buffer_newValue
+);
+    always @(*) begin
+        if(buffer_value[35]) begin // 如果数据已经就绪，不读取 cdb
+            buffer_newValue = buffer_value;
+        end
+        else begin // 如果数据未就绪，试图读取 cdb
+            if(cdb_buzy && cdb_device == buffer_value[34:32]) begin // cdb 输出的是我想要读取的
+                //! 此处一定要记得判断 cdb_buzy
+                buffer_newValue[35]    = 1;
+                buffer_newValue[34:32] = 0;
+                buffer_newValue[31: 0] = cdb_value;
+            end
+            else begin // cdb 输出的不是我想要读取的
+                buffer_newValue <= buffer_value;
+            end
+        end
+    end
+endmodule
+
+
 // -------------------- 所有执行部件以及其编号 -------------------- //
+//! 使用集中控制的方法确定哪一个部件可以输出到 CDB
+//! 当有多个部件同时想要占据 CDB 时，优先允许编号较小的部件占用 CDB
 `define DEVICE_ADDER 1 // 加减运算器
 `define DEVICE_LOGIC 2 // 逻辑运算器
 `define DEVICE_DM    3 // 数据存储器
@@ -49,7 +91,7 @@ module BufferIn (
     
     // 以下三个端口数据来自 CDB(通用数据总线)
     input       cdb_buzy,    // CDB 忙
-    input[2 :0] cbd_device,  // CDB 的输出设备
+    input[2 :0] cdb_device,  // CDB 的输出设备
     input[31:0] cdb_value,   // CDB 的输出值
 
     // 以下端口来自计算机基本框架
@@ -68,6 +110,12 @@ module BufferIn (
 );
     assign out_ready = (out_valueA[35] && out_valueB[35]); //! 两个数据都就绪，则可进行加法
 
+    //! fetch 模块保证在数据传输过程中也能正确读取 CDB 中的值
+    wire [35:0] fetchCDB_valueA;
+    FetchCDB U_FetchCDB_valueA(cdb_buzy, cdb_device, cdb_value, in_valueA, fetchCDB_valueA);
+    wire [35:0] fetchCDB_valueB;
+    FetchCDB U_FetchCDB_valueB(cdb_buzy, cdb_device, cdb_value, in_valueB, fetchCDB_valueB);
+
     always @(posedge clk) begin
         if(rst) begin 
             out_buzy      <= 0; // 用于启动时的清空操作
@@ -85,12 +133,12 @@ module BufferIn (
                 end
                 if(!out_ready) begin //? 如果数据未就绪，在 CDB 监听 valueA 和 valueB
                     if(cdb_buzy) begin
-                        if(!out_valueA[35] && out_valueA[34:32] == cbd_device) begin //! 监听数据 A
+                        if(!out_valueA[35] && out_valueA[34:32] == cdb_device) begin //! 监听数据 A
                             out_valueA[35]    <= 1;
                             out_valueA[34:32] <= 0;
                             out_valueA[31: 0] <= cdb_value;
                         end
-                        if(!out_valueB[35] && out_valueB[34:32] == cbd_device) begin //! 监听数据 B
+                        if(!out_valueB[35] && out_valueB[34:32] == cdb_device) begin //! 监听数据 B
                             out_valueB[35]    <= 1;
                             out_valueB[34:32] <= 0;
                             out_valueB[31: 0] <= cdb_value;
@@ -103,8 +151,8 @@ module BufferIn (
                 if(in_hasInput && in_device == device_now) begin
                     out_buzy      <= 1;
                     out_algorithm <= in_algorithm;
-                    out_valueA    <= in_valueA;
-                    out_valueB    <= in_valueB;
+                    out_valueA    <= fetchCDB_valueA; //? 此处传输已经进行了更正
+                    out_valueB    <= fetchCDB_valueB;
                 end
             end
         end
@@ -159,6 +207,7 @@ module AdderBufferOut (
     output reg [2 :0] out_device, // 加法器设备编号
     output reg [31:0] out_value   // 加法器结果
 );
+    //! 在未来的版本可能会在此处增加对溢出的处理
     wire [31:0] adderAnswer;
     AdderComb U_AdderComb(in_valueA[31:0], in_valueB[31:0], in_algorithm, adderAnswer); // 加法的组合逻辑电路
 
@@ -211,7 +260,7 @@ endmodule
 
 // -------------------- 逻辑运算输出暂存器 -------------------- //
 module LogicBufferOut (
-    // 以下五个端口来自逻辑缓冲站
+    // 以下五个端口来自逻辑缓冲站 //! 加法缓冲站和乘法缓冲站均使用 BufferIn 模块
     input        in_buzy,
     input        in_ready,
     input [1 :0] in_algorithm,
@@ -274,11 +323,11 @@ module DataMemory (
     input[35:0] in_valueB,    // 要写入的数据值
     input       in_DM_write,  // 是否写数据存储器，不写说明是读
     input       in_DM_sign,   // 读出数据是否带符号拓展
-    input[15:0] in_DM_offset,    // 地址偏移量 (需要带符号拓展)
+    input[15:0] in_DM_offset, // 地址偏移量 (需要带符号拓展)
     
     // 以下三个端口数据来自 CDB(通用数据总线) //! 输入输出缓冲站也需要等待 CDB 中的数据
     input       cdb_buzy,
-    input[2 :0] cbd_device,
+    input[2 :0] cdb_device,
     input[31:0] cdb_value,
 
     // 以下端口来自计算机基本框架
@@ -303,9 +352,15 @@ module DataMemory (
     reg        buffer_DM_sign;
     reg [15:0] buffer_DM_offset; // 偏移地址
 
+    //! 写内存时要求两个值都需要就绪，读内存时第二个值没有意义
     wire buffer_ready;
     assign buffer_ready = buffer_DM_write ? buffer_valueA[35] && buffer_valueB[35] : buffer_valueA[35];
-    //! 写内存时要求两个值都需要就绪，读内存时第二个值没有意义
+
+    //! fetch 模块保证在数据传输过程中也能正确读取 CDB 中的值
+    wire [35:0] fetchCDB_valueA;
+    FetchCDB U_FetchCDB_valueA(cdb_buzy, cdb_device, cdb_value, in_valueA, fetchCDB_valueA);
+    wire [35:0] fetchCDB_valueB;
+    FetchCDB U_FetchCDB_valueB(cdb_buzy, cdb_device, cdb_value, in_valueB, fetchCDB_valueB);
 
     wire [31:0] buffer_addr; // 地址
     assign buffer_addr = buffer_valueA[31:0] + {{16{buffer_DM_offset[15]}}, buffer_DM_offset}; // 带符号拓展
@@ -320,6 +375,7 @@ module DataMemory (
         end
         else begin
             if(buffer_buzy && buffer_ready && buffer_DM_write) begin // 可以写入存储器
+                //! 存储器使用小端方式存储 32 位字
                 case(buffer_algorithm)
                     `DM_ALGO_BYTE: begin
                         DM[buffer_addr + 0] <= buffer_valueB[7:0];
@@ -377,12 +433,12 @@ module DataMemory (
                     end
                 end
                 else begin // 数据未就绪
-                    if(!buffer_valueA[35] && cbd_device == buffer_valueA[34:32]) begin // 可以读入数据 A
+                    if(!buffer_valueA[35] && cdb_device == buffer_valueA[34:32]) begin // 可以读入数据 A
                         buffer_valueA[35]    <= 1;
                         buffer_valueA[34:32] <= 0;
                         buffer_valueA[31: 0] <= cdb_value;
                     end
-                    if(buffer_DM_write && !buffer_valueB[35] && cbd_device == buffer_valueB[34:32]) begin // 可以读入数据 B
+                    if(buffer_DM_write && !buffer_valueB[35] && cdb_device == buffer_valueB[34:32]) begin // 可以读入数据 B
                         //! 读存储器不需要数据 B
                         buffer_valueB[35]    <= 1;
                         buffer_valueB[34:32] <= 0;
@@ -394,8 +450,8 @@ module DataMemory (
                 if(in_hasInput && in_device == `DEVICE_DM) begin
                     buffer_buzy      <= 1;
                     buffer_algorithm <= in_algorithm;
-                    buffer_valueA    <= in_valueA;
-                    buffer_valueB    <= in_valueB;
+                    buffer_valueA    <= fetchCDB_valueA;
+                    buffer_valueB    <= fetchCDB_valueB; //? 此处传输已经得到了更正
                     buffer_DM_write  <= in_DM_write;
                     buffer_DM_sign   <= in_DM_sign;
                     buffer_DM_offset <= in_DM_offset;
@@ -420,7 +476,7 @@ module DataMemory (
                             out_value <= {{24{buffer_DM_sign ? DM[buffer_addr][7] : 1'b0}}, DM[buffer_addr]};
                         end
                         `DM_ALGO_HALF: begin
-                            out_value <= {{24{buffer_DM_sign ? DM[buffer_addr + 1][7] : 1'b0}}, DM[buffer_addr + 1], DM[buffer_addr + 0]};
+                            out_value <= {{16{buffer_DM_sign ? DM[buffer_addr + 1][7] : 1'b0}}, DM[buffer_addr + 1], DM[buffer_addr + 0]};
                         end
                         `DM_ALGO_WORD: begin //! 加载字的时候是否带符号拓展无关紧要
                             out_value <= {DM[buffer_addr + 3], DM[buffer_addr + 2], DM[buffer_addr + 1], DM[buffer_addr + 0]};
@@ -439,13 +495,212 @@ module DataMemory (
 endmodule
 
 
+// -------------------- 跳转器 -------------------- //
+//! 如果跳转器正在工作，那么 IFU 不可以取出下一条指令
+//! PC 从 CDB 获取跳转器输出的跳转结果
+//! 如果 CDB 没有输出跳转结果，则 PC 默认 +4
+`define JMP_ALGO_JMP   1 // 无条件跳转
+`define JMP_ALGO_BEQ   2 // valueA =  valueB 跳转
+module Jmp (
+    // 以下端口来自指令发射缓冲器
+    //! 跳转的目标地址可能是寄存器的值
+    //! 也可能是一个可以通过 PC+4 和已知偏移量计算出的数
+    //! 执行当前指令时，PC 的值已经 +4
+    input        in_hasInput,
+    input [2 :0] in_device,
+    input [35:0] in_targetAddr,
+    input [1 :0] in_algorithm,
+    input [35:0] in_valueA,
+    input [35:0] in_valueB,
+
+    // 以下三个端口数据来自 CDB(通用数据总线) //! 输入输出缓冲站也需要等待 CDB 中的数据
+    input       cdb_buzy,
+    input[2 :0] cdb_device,
+    input[31:0] cdb_value,
+
+    // 以下端口来自计算机基本框架
+    input clk,
+    input rst,
+
+    // 以下端口来自后继设备 (CDB 是暂存器的后继设备)
+    input dvc_adder_buzy,
+    input dvc_logic_buzy,
+    input dvc_memory_buzy, //! 跳转模块只有在没人想要占用 CDB 时才能占用 CDB
+    
+    // 输出端口
+    output reg        buffer_buzy,
+    output reg        out_buzy,    // 暂存器是否要输出 //! 一定要注意，设备能接受输入的条件是保留站中没有元素
+    output reg [2 :0] out_device,  // 存储器的设备号
+    output reg [31:0] out_value    // 存储器中读取的数据
+);
+    //! 类似存储器，内置了一个保留站
+    reg [35:0] buffer_targetAddr;
+    reg [1 :0] buffer_algorithm;
+    reg [35:0] buffer_valueA;
+    reg [35:0] buffer_valueB;
+
+    reg buffer_ready; //! 实际上会被综合成组合逻辑，无条件跳转不需要等待 valueA 与 valueB
+    always @(*) begin
+        if(buffer_algorithm == `JMP_ALGO_BEQ) buffer_ready = buffer_targetAddr[35] && buffer_valueA[35] && buffer_valueB[35]; else
+        if(buffer_algorithm == `JMP_ALGO_JMP) buffer_ready = buffer_targetAddr[35]; else buffer_ready = 1; // 不跳转
+    end
+
+    //! fetch 模块保证在数据传输过程中也能正确读取 CDB 中的值
+    wire [35:0] fetchCDB_valueA;
+    FetchCDB U_FetchCDB_valueA(cdb_buzy, cdb_device, cdb_value, in_valueA, fetchCDB_valueA);
+    wire [35:0] fetchCDB_valueB;
+    FetchCDB U_FetchCDB_valueB(cdb_buzy, cdb_device, cdb_value, in_valueB, fetchCDB_valueB);
+    wire [35:0] fetchCDB_targetAddr;
+    FetchCDB U_FetchCDB_targetAddr(cdb_buzy, cdb_device, cdb_value, in_targetAddr, fetchCDB_targetAddr);
+
+    // 以下为保留站寄存器行为逻辑
+    always @(posedge clk) begin
+        if(rst) begin
+            buffer_buzy       <= 0;
+            buffer_targetAddr <= 0;
+            buffer_algorithm  <= 0;
+            buffer_valueA     <= 0;
+            buffer_valueB     <= 0;
+        end
+        else begin
+            if(buffer_buzy) begin
+                if(buffer_ready) begin // 如果缓冲区的数据已经准备就绪了，试图输出到下一个部件
+                    if(!out_buzy) begin
+                        buffer_buzy       <= 0; // 当前部件只需要考虑清空问题
+                        buffer_targetAddr <= 0;
+                        buffer_valueA     <= 0;
+                        buffer_valueB     <= 0;
+                    end
+                end
+                else begin // 如果数据未就绪，则在 CDB 监听
+                    if(!buffer_targetAddr[35] && buffer_targetAddr[34:32] == cdb_device) begin // 监听 target
+                        buffer_targetAddr[35]    <= 1;
+                        buffer_targetAddr[34:32] <= 0;
+                        buffer_targetAddr[31: 0] <= cdb_value;
+                    end
+                    //! 虽然有些情况 valueA, valueB不需要监听，但是我觉得监听一下也没问题
+                    if(!buffer_valueA[35] && buffer_valueA[34:32] == cdb_device) begin
+                        buffer_valueA[35]    <= 1;
+                        buffer_valueA[34:32] <= 0;
+                        buffer_valueA[31: 0] <= cdb_value;
+                    end
+                    if(!buffer_valueB[35] && buffer_valueB[34:32] == cdb_device) begin
+                        buffer_valueB[35]    <= 1;
+                        buffer_valueB[34:32] <= 0;
+                        buffer_valueB[31: 0] <= cdb_value;
+                    end
+                end
+            end
+            else begin // 如果缓冲区中没有数据，试图从输入读入数据
+                if(in_hasInput && in_device == `DEVICE_JMP) begin
+                    buffer_buzy       <= 1;
+                    buffer_targetAddr <= fetchCDB_targetAddr; //? 此处赋值已更正
+                    buffer_algorithm  <= in_algorithm;
+                    buffer_valueA     <= fetchCDB_valueA;
+                    buffer_valueB     <= fetchCDB_valueB; //? 此处赋值已更正
+                end
+            end 
+        end
+    end
+
+    always @(posedge clk) begin
+        if(rst) begin
+            out_buzy    <= 0;
+            out_device  <= `DEVICE_JMP;
+            out_value   <= 0;
+        end
+        else begin
+            if(out_buzy) begin // 如果数据准备好了，将要输出
+                if(!cdb_buzy && !dvc_adder_buzy && !dvc_logic_buzy && !dvc_memory_buzy) begin // 没人想抢占才不忙
+                    out_buzy  <= 0;
+                    out_value <= 0;
+                end
+            end
+            else begin // 还没有准备好，试图从保留站输入
+                if(buffer_buzy && buffer_ready) begin
+                    case(buffer_algorithm)
+                        `JMP_ALGO_BEQ: begin
+                            out_buzy  <= (buffer_valueA[31:0] == buffer_valueB[31:0]); // 跳转不成功，则不上 CDB
+                            out_value <= (buffer_valueA[31:0] == buffer_valueB[31:0]) ? buffer_targetAddr[31:0] : 0;
+                        end
+                        `JMP_ALGO_JMP: begin
+                            out_buzy  <= 1;
+                            out_value <= buffer_targetAddr[31:0];
+                        end
+                        //! default 保持不变
+                    endcase
+                end
+            end
+        end
+    end
+endmodule
+
+
+// -------------------- 寄存器堆 -------------------- //
+//! 读寄存器使用组合逻辑
+//! 修改寄存器必须和指令发射同步完成，指令不发射，就不能修改寄存器
+`define GPR_SIZE 32
+module GPR (
+    // 以下内容来自指令发射缓冲器
+    input        in_buzy,
+    input        in_write,
+    input [4 :0] in_writeId,
+    input [35:0] in_writeValue, // 这里的 writeValue 可能是指出设备，或者直接给出常量
+    input [4 :0] in_readIdA,
+    input [4 :0] in_readIdB, // 任何指令都必须读两个寄存器中的值
+
+    input in_launch, //! 这一瞬间指令是否能够成功发射
+
+    // 以下端口来自计算机基本结构
+    input clk,
+    input rst,
+
+    // 以下内容来自 cdb
+    input        cdb_buzy,
+    input [2 :0] cdb_device,
+    input [31:0] cdb_value,
+
+    // 以下内容为输出 //! 目前我认为，此处输出不需要检查 CDB, 因为之后的元件都会检查
+    output [35:0] out_readA,
+    output [35:0] out_readB
+);
+    integer i;
+
+
+    reg [35:0] registers [0 : `GPR_SIZE - 1]; // 寄存器数组
+    always @(posedge clk) begin // 描述寄存器数组的写入行为
+        if(rst) begin
+            for(i = 0; i < `GPR_SIZE; i += 1) begin
+                registers[i] <= {1'b1, 3'b000, 32'h00000000}; //! 初始化时，数据均为就绪的 0
+            end
+        end
+        else begin
+            for(i = 1; i < `GPR_SIZE; i += 1) begin // 监听 CDB, 0号寄存器不可以写
+                if(in_launch && in_writeId == i) begin // 只有发射同时才能写寄存器
+                    registers[in_writeId] = in_writeValue; //! 此处一定要注意，此处绝对不能读 CDB
+                    //! 就算这个时刻 CDB 上有对应元件输出的数据，也不能读，因为当前指令才刚发射
+                end
+                else begin // 此处一定要记得判断 cdb_buzy
+                    if(!registers[i][35] && cdb_buzy && registers[i][34:32] == cdb_device) begin
+                        registers[i][35]    <= 1;
+                        registers[i][34:32] <= 0;
+                        registers[i][31: 0] <= cdb_value;
+                    end
+                end
+            end
+        end
+    end
+
+    // 描述数据读取的行为
+    assign out_readA = registers[in_readIdA];
+    assign out_readB = registers[in_readIdB]; // 使用组合逻辑读取寄存器中的数据
+endmodule
+
+
 //! -------------------- 以下内容尚未完成 -------------------- //
-// 跳转器
-// 寄存器堆
 
 // 取指令模块    //! 注：PC 也要监听 CDB 中设备 DEVICE_JMP 的输出
-//! 取指令模块中有 jumpState 状态位，用于控制流水线阻塞（锁 PC 和 原指令缓冲器）
-//! DEVICE_JMP 上 CDB 的同时，清 jumpState 状态
+//! jumpState: 有指令在缓冲，JMP模块在工作，CDB 上在发送 JMP 的结果
 //! 换言之，PC 会保持在 跳转指令的下一条指令处，原指令缓冲器保持在无效状态
 
 // MIPS 总电路图
